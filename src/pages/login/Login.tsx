2c6c1d86a54c35a53fd1f5d9a66664ddc5912ee8
DKULena <32173057@dankook.ac.kr>
2024-12-03 15:25:01 +0900

    add:loginSetting

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import './Login.scss';
 
 interface LoginProps {
-    //
-};
+  //
+}
 
 const Login = ({}: LoginProps) => {
-    return(
-        <>Login</>
-    );
-}
+  return (
+    <section className="loginPage">
+      <h1>핸드펫이 오신 것을 환영합니다 :)</h1>
+      <section className="convinientLogin">
+        <ul>
+          <li className="kakaoLogin">
+            <a href="#">카카오톡 계정으로 로그인</a>
+          </li>
+          <li className="googleLogin">
+            <a href="#">구글 계정으로 로그인</a>
+          </li>
+          <li className="naverLogin">
+            <a href="#">네이버 계정으로 로그인</a>
+          </li>
+        </ul>
+      </section>
+      <section className="emailLoginAndSignUp">
+        <ul>
+            <li><a href="#">이메일로 로그인</a></li>
+            <li><a href="#">회원가입</a></li>
+        </ul>
+      </section>
+    </section>
+  );
+};
 
 export default Login;
