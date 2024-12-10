e9f80f80b3c0edfc687866d4c41f774ed4497fd2
DKULena <32173057@dankook.ac.kr>
2024-12-10 22:10:04 +0900

    add: email login setting

--- src/pages/login/SignUpEmail.tsx
+++ src/pages/login/SignUpEmail.tsx
@@ -7,16 +7,14 @@ interface SignUpEmailProps {
 
 const Login = ({}: SignUpEmailProps) => {
   return (
-    <div className="App" style={ { fontSize: '120px'}}>
-    <h1>이메일로 회원가입</h1>
-    <form action="#">
-        <input type="email" placeholder="이메일 주소를 입력해주세요."/>
-        <input type="password" placeholder="비밀번호를 설정해 주세요."/>
-        <button style={ {border: '1px solid black'}}>
-            다음
-        </button>
-    </form>
-    </div>
+    <section className="SignUp">
+      <h1>이메일로 회원가입</h1>
+      <form action="#" className="signUpEmail">
+        <input className="emailInput" type="email" placeholder="이메일 주소를 입력해주세요." />
+        <input className="pwInput" type="password" placeholder="비밀번호를 설정해 주세요." />
+        <button className="nextBtn">다음</button>
+      </form>
+    </section>
   );
 };
 
