b939de7b1daebcc8f4d2fc35428da737621f0c97
DKULena <32173057@dankook.ac.kr>
2024-12-18 14:06:40 +0900

    add: profileSetting

--- src/pages/login/FirstProfileSetting.tsx
+++ src/pages/login/FirstProfileSetting.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./FirstProfileSetting.scss";
+import IconImage from '../../assets/icons/icon-profile.svg'
+import IconImageSetting from '../../assets/icons/icon-profileSetting.svg'
 
 interface FirstProfileSettingProps {
   //
@@ -7,7 +10,32 @@ interface FirstProfileSettingProps {
 
 const FirstProfileSetting = ({}: FirstProfileSettingProps) => {
   return (
-    <div>Hi</div>
+    <section className="profileContainer">
+      <h1>프로필 설정</h1>
+      <span>나중에 언제든 변경할 수 있습니다.</span>
+      <div className="profileImg Cont">
+        <img src={IconImage} alt="기본 강아지 이모지" />
+        {/* <img src={IconImageSetting} alt="이미지 변경 아이콘" /> */}
+      </div>
+      <div className="userInfo Cont">
+        <div className="userName">
+          <div className="userName Title">사용자 이름</div>
+          <input type="text" className="userName Input" placeholder="2~10자 이내여야 합니다." />
+        </div>
+        <div className="accountId Cont">
+          <div className="accountId Title">계정 ID</div>
+          <input type="text" className="accountId Input" placeholder="영문, 숫자, 특수문자(.)/(_)만 사용 가능합니다." />
+          <div className="wrongType">* 이미 사용 중인 ID입니다.</div>
+        </div>
+        <div className="intro Cont">
+        <div className="intro Title">소개</div>
+        <input type="text" className="intro Input" placeholder="자신과 판매할 상품에 대해 소개해 주세요." />
+        </div>
+        <Link to="/home" className="nextBtn">시작하기</Link>
+        {/* <div className="nextBtn">시작하기</div> */}
+      </div>
+
+    </section>
   );
 };
 
--- src/pages/login/SignUpEmail.tsx
+++ src/pages/login/SignUpEmail.tsx
@@ -12,16 +12,26 @@ const SignUpEmail = ({}: SignUpEmailProps) => {
       <h1>이메일로 회원가입</h1>
       <form action="#" className="signUpEmail">
         <div id="emailSection">
-        <input className="emailInput" type="email" placeholder="이메일 주소를 입력해주세요." />
-        <div className="wrongType">* 이메일 형식이 올바르지 않습니다.</div>
-        <div className="wrongType">* 이미 가입된 이메일 주소입니다.</div>
+          <input
+            className="emailInput"
+            type="email"
+            placeholder="이메일 주소를 입력해주세요."
+          />
+          <div className="wrongType">* 이메일 형식이 올바르지 않습니다.</div>
+          {/* <div className="wrongType">* 이미 가입된 이메일 주소입니다.</div> */}
         </div>
         <div className="pwSection">
-        <input className="pwInput" type="password" placeholder="비밀번호를 설정해 주세요." />
-        <div className="wrongType">* 비밀번호는 영문, 숫자를 포함하여 8자 이상이어야 합니다.</div>
+          <input
+            className="pwInput"
+            type="password"
+            placeholder="비밀번호를 설정해 주세요."
+          />
+          <div className="wrongType">
+            * 비밀번호는 영문, 숫자를 포함하여 8자 이상이어야 합니다.
+          </div>
         </div>
-        {/* <Link to="/signUpEmail" id="signUp">회원가입</Link> */}
-        <button className="nextBtn">다음</button>
+        <Link to="/FirstProfileSetting" className="nextBtn" id="signUp">다음</Link>
+        {/* <button className="nextBtn">다음</button> */}
       </form>
     </section>
   );
